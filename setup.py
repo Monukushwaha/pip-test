29402379d4f61870822516481acb924d60318c85
“championshuttler” <shivams2799@gmail.com>
2022-05-30 17:34:23 +0530

    fixed path

--- setup.py
+++ setup.py
@@ -4,18 +4,18 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='pip-test',
+    name='heytest',
     version='0.0.3',
     author='Monukushwaha',
     author_email='monu17@navgurukul.org',
     description='Testing installation of Package',
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url='https://github.com/Monukushwaha/pip-test',
+    url='https://github.com/Monukushwaha/pip-test/',
     project_urls = {
         "Bug Tracker": "https://github.com/Monukushwaha/pip-test/issues"
     },
     license='MIT',
-    packages=['pip-test'],
+    packages=['heytest'],
     install_requires=['requests'],
 )
